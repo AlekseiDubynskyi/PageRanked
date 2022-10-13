14d214e063d471185bb0da9fab925640510577ae
AlekseiDubynskyi <dubynskyialeksei@gmail.com>
2022-10-13 11:48:17 +0300

    Add HTML parser

--- src/main/java/com/solvd/pageranked/services/test/HTMLParser.java
+++ src/main/java/com/solvd/pageranked/services/test/HTMLParser.java
@@ -4,20 +4,27 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.Objects;
 
 
 public class HTMLParser {
     private static final Logger LOGGER = LogManager.getLogger(HTMLParser.class);
+
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.parse(new File("src/main/resources/html/Third.html"), "UTF-8");
-        String htmlFile = null;
-        Elements link = doc.select("a");
-        htmlFile = link.attr("href");
-
-
-        System.out.println(htmlFile);
+        File folder = new File("src/main/resources/html/");
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
+            System.out.println("\n" + "For " + file.getName() + ":");
+            Document doc = Jsoup.parse(new File(folder + "/" + file.getName()), "UTF-8");
+            Elements links = doc.select("a");
+            for (Element link : links) {
+                String linkHref = link.attr("href");
+                String linkText = link.text();
+                System.out.println(linkHref + " - " + linkText);
+            }
+        }
     }
 }
